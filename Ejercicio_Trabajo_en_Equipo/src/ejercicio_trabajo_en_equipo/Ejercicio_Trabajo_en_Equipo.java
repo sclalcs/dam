1eed93fc0c1d43fcb3c3cceb9f1d7d5b42116665
alcs <alcslpa@gmail.com>
2016-05-22 10:56:04 +0100

    Creado proyecto Ejercicio_Trabajo_en_Equipo, he copiado los ficheros en ese proyecto y he hecho un add y ahora hago un commit

---
+++ Ejercicio_Trabajo_en_Equipo/src/ejercicio_trabajo_en_equipo/Ejercicio_Trabajo_en_Equipo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_trabajo_en_equipo;
+
+/**
+ *
+ * @author ALCS
+ */
+public class Ejercicio_Trabajo_en_Equipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Ejercicio_Trabajo_en_Equipo/src/ejercicio_trabajo_en_equipo/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicio_trabajo_en_equipo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta Cuenta1; 
+                
+               
+        double saldoActual;
+  
+
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        Cuenta1.operativa_Cuenta(2500,500);
+        saldoActual = Cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("Esto es para el repositorio remoto");
+        System.out.println("Esto es otro para el repositorio remoto");
+        System.out.println("Mandar repositorio desde proyecto");
+        
+        
+    }
+}
